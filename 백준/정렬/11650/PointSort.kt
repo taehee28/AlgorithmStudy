1e7f17eec25bbb895caffd5690c4b6a75e23ccf6
Taehee Kang <49335498+taehee28@users.noreply.github.com>
2022-09-19 22:07:35 +0900

    add: 백준 11650 풀이

---
+++ 백준/정렬/11650/PointSort.kt
@@ -0,0 +1,21 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val count = br.readLine().toInt()
+
+    val list = mutableListOf<Pair<Int, Int>>()
+    repeat(count) {
+        val (x, y) = br.readLine().split(" ")
+        list.add(x.toInt() to y.toInt())
+    }
+
+    // 느리다
+    list.sortWith(compareBy<Pair<Int, Int>> { it.first }.thenBy { it.second })
+
+    val sb = StringBuilder()
+    list.forEach { sb.append(it.first).append(" ").append(it.second).append("\n") }
+
+    print(sb.toString())
+}

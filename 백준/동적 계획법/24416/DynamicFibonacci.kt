eb27ac6c67022cc3a3735456ae72f705e1791a41
Taehee Kang <49335498+taehee28@users.noreply.github.com>
2023-01-10 14:01:37 +0900

    Add: 백준 24416(알고리즘 수업 - 피보나치 수 1) 풀이

---
+++ 백준/동적 계획법/24416/DynamicFibonacci.kt
@@ -0,0 +1,36 @@
+package dynamicprogramming
+
+fun main () {
+    val number = readln().toInt()
+    val sb = StringBuilder()
+    sb.append(recursiveFib(number)).append(" ").append(dynamicFib(number))
+    print(sb.toString())
+}
+
+private fun recursiveFib(count: Int): Int {
+    var callCount = 0
+
+    fun fib(n: Int): Int = if (n <= 2) {
+        ++callCount
+        1
+    } else {
+        fib(n - 1) + fib(n - 2)
+    }
+
+    fib(count)
+    return callCount
+}
+
+private fun dynamicFib(count: Int): Int {
+    var callCount = 0
+    val map = mutableMapOf(1 to 1, 2 to 1)
+
+    for (i in 3..count) {
+        map.getOrPut(i) {
+            ++callCount
+            map[i - 1]!! + map[i - 2]!!
+        }
+    }
+
+    return callCount
+}
